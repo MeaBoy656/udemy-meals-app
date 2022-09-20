5ec473340878aa7b1049624fe062e98d2da040a4
MeaBoy656 <MeaBoy656@gmail.com>
2022-09-20 23:05:54 +0300

    Added Meals related components

---
+++ src/components/Meals/MealItem/MealsItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MealItemForm from "./MealItemForm";
+import classes from "./MealsItem.module.css";
+
+const MealsItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3 className={classes.name}>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id}/>
+      </div>
+    </li>
+  );
+};
+
+export default MealsItem;
